76b9c25f4e10eb65a6a57884f8dd9b594b99526b
Jackson Currie <j.currie1999@gmail.com>
2021-06-23 23:45:55 +1200

    Update for new database

--- app/app.js
+++ app/app.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors');
-const schema = require('./graphql/schema.js');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const schema = require("./graphql/schema.js");
 
 const localUrl = "mongodb://localhost:27017";
-const serverUrl = "mongodb+srv://dbUser:user123@execute-vxpji.mongodb.net";
+const serverUrl =
+  "mongodb+srv://jackson:S3XpnmNnOaWNbrZY@execute.ja3x6.mongodb.net/execute?retryWrites=true&w=majority";
 
-MongoClient.connect(serverUrl, { useNewUrlParser: true }, (err, db) => {
+MongoClient.connect(
+  serverUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, db) => {
     if (err) throw err;
     global.users = db.db("execute").collection("userData");
     console.log("Connected to database");
-});
+  }
+);
 
 const app = express();
 
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     graphiql: true,
-}));
+  })
+);
 
-app.listen(8080, () => console.log('Server running on port 8080'));
+app.listen(8080, () => console.log("Server running on port 8080"));
